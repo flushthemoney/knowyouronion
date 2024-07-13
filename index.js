15e6b06e5c60f9996f40e9650ea99a8c2ee2010c
kaviraj <skaviraj562@gmail.com>
2024-07-13 20:52:52 +0530

    setup tailwind, implement initial design
    Signed-off-by: kaviraj <skaviraj562@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const 
+const stateList = document.querySelector("#stateList");
+const stateSelector = document.querySelector("#stateSelector");
 
-const url =
-  "https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd00000128b62a2b46904c986bb81d02c47919f7&format=json";
-
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+stateSelector.addEventListener("click", () => {
+  stateList.classList.toggle("hidden");
+});
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import json
 import pandas as pd
 
-# Sample data
 with open('test-knowyouronion.json', 'r') as f:
     data = json.load(f)
 
-# Convert the list of dictionaries to a DataFrame
 df = pd.DataFrame(data)
 
-# Function to create the nested dictionary
 def create_nested_dict(df):
     result = {}
     for state, state_df in df.groupby('state'):
@@ -21,13 +18,10 @@ def create_nested_dict(df):
                     result[state][district][market][commodity] = commodity_df.to_dict(orient='records')
     return result
 
-# Create the nested dictionary
 nested_dict = create_nested_dict(df)
-
-# Convert the dictionary to JSON
 nested_json = json.dumps(nested_dict, indent=4)
 
-# Write to a file
+
 with open('nested_data.json', 'w') as f:
     f.write(nested_json)
 
