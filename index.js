aac2e6f69a53e40c8e467014ea55462e48abc5b8
kaviraj <skaviraj562@gmail.com>
2024-07-14 21:38:55 +0530

    add state-dropdown list from local json
    Signed-off-by: kaviraj <skaviraj562@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const stateList = document.querySelector("#stateList");
-const stateSelector = document.querySelector("#stateSelector");
+window.onload = function () {
+  const stateList = document.querySelector("#stateList");
+  const stateSelector = document.querySelector("#stateSelector");
 
-stateSelector.addEventListener("click", () => {
-  stateList.classList.toggle("hidden");
-});
+  fetch("./nested_data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const listofStates = Object.keys(data);
+      listofStates.forEach((state) => {
+        console.log(state);
+        const listItem = document.createElement("li");
+        listItem.className = "text-xl py-0.5 px-5 hover:bg-gray-100";
+        listItem.textContent = state;
+        stateList.appendChild(listItem);
+      });
+    });
+
+  stateSelector.addEventListener("click", () => {
+    stateList.classList.toggle("hidden");
+  });
+};
