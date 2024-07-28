ddd6a9af8d0b0624fc07ae3bcc10bd3dc09ed44f
kaviraj <skaviraj562@gmail.com>
2024-07-28 22:02:29 +0530

    districtList progress
    Signed-off-by: kaviraj <skaviraj562@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 window.onload = function () {
   const stateList = document.querySelector("#stateList");
   const stateSelector = document.querySelector("#stateSelector");
+  const stateInput = "";
+  const districtList = document.querySelector("#districtList");
+  const districtSelector = document.querySelector("#districtSelector");
+  var selectedState = document.querySelector("#stateSearch");
 
   fetch("./nested_data.json")
     .then((res) => res.json())
     .then((data) => {
       const listofStates = Object.keys(data);
       listofStates.forEach((state) => {
-        console.log(state);
         const listItem = document.createElement("li");
         listItem.className = "text-xl py-0.5 px-5 hover:bg-gray-100";
         listItem.textContent = state;
@@ -18,4 +21,29 @@ window.onload = function () {
   stateSelector.addEventListener("click", () => {
     stateList.classList.toggle("hidden");
   });
+  stateList.addEventListener("click", function (event) {
+    selectedState = event.target.innerHTML;
+    console.log(event.target.innerHTML);
+    console.log(selectedState);
+  });
+
+  districtSelector.addEventListener("click", () => {
+    districtList.classList.toggle("hidden");
+  });
+
+  fetch("./nested_data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const stateData = data[selectedState];
+      const listofDistricts = Object.keys(stateData);
+      listofDistricts.forEach((district) => {
+        const listItem = document.createElement("li");
+        listItem.className = "text-xl py-0.5 px-5 hover:bg-gray-100";
+        listItem.textContent = district;
+        districtList.appendChild(listItem);
+        console.log(listItem);
+      });
+    });
+
+  console.log(selectedState);
 };
