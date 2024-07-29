84371d38b871cce97f3a5bd9fdb3e010390a4e81
kaviraj <skaviraj562@gmail.com>
2024-07-29 21:48:38 +0530

    districtList complete
    Signed-off-by: kaviraj <skaviraj562@gmail.com>

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+function clearList(list) {
+  const ul = list;
+  while (ul.firstChild) {
+    ul.removeChild(ul.firstChild);
+    console.log("Hey");
+  }
+}
+
 window.onload = function () {
   const stateList = document.querySelector("#stateList");
   const stateSelector = document.querySelector("#stateSelector");
   const stateInput = "";
   const districtList = document.querySelector("#districtList");
   const districtSelector = document.querySelector("#districtSelector");
-  var selectedState = document.querySelector("#stateSearch");
+  var selectedState;
 
   fetch("./nested_data.json")
     .then((res) => res.json())
@@ -22,28 +30,30 @@ window.onload = function () {
     stateList.classList.toggle("hidden");
   });
   stateList.addEventListener("click", function (event) {
+    clearList(districtList);
     selectedState = event.target.innerHTML;
-    console.log(event.target.innerHTML);
-    console.log(selectedState);
+    stateSelector.textContent = selectedState;
+    stateList.classList.toggle("hidden");
+    fetch("./nested_data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const stateData = data[selectedState];
+        const listofDistricts = Object.keys(stateData);
+        listofDistricts.forEach((district) => {
+          const listItem = document.createElement("li");
+          listItem.className = "text-xl py-0.5 px-5 hover:bg-gray-100";
+          listItem.textContent = district;
+          districtList.appendChild(listItem);
+          console.log(listItem.textContent);
+        });
+      });
   });
 
   districtSelector.addEventListener("click", () => {
-    districtList.classList.toggle("hidden");
+    if (selectedState != null) {
+      districtList.classList.toggle("hidden");
+    }
   });
-
-  fetch("./nested_data.json")
-    .then((res) => res.json())
-    .then((data) => {
-      const stateData = data[selectedState];
-      const listofDistricts = Object.keys(stateData);
-      listofDistricts.forEach((district) => {
-        const listItem = document.createElement("li");
-        listItem.className = "text-xl py-0.5 px-5 hover:bg-gray-100";
-        listItem.textContent = district;
-        districtList.appendChild(listItem);
-        console.log(listItem);
-      });
-    });
 
   console.log(selectedState);
 };
