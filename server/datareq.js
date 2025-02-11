8e352066d98afb19abbbd91175c3771bc649069b
flushthemoney <skaviraj562@gmail.com>
2025-02-11 21:26:46 +0530

    seperate fe & be
    Signed-off-by: flushthemoney <skaviraj562@gmail.com>

---
+++ server/datareq.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
+
+async function fetchDataAndSave() {
+  try {
+    const apiUrl = `${process.env.URL}`;
+
+    const response = await axios.get(apiUrl);
+
+    const jsonData = response.data;
+
+    const records = jsonData.records || [];
+
+    const jsonString = JSON.stringify(records, null, 2);
+    const filePath = "data.json";
+
+    fs.writeFile(filePath, jsonString, (err) => {
+      if (err) {
+        console.error("Error writing file:", err);
+      } else {
+        console.log("JSON data saved to", filePath);
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching or saving data:", error);
+    if (error.response) {
+      console.error("Response Error Data:", error.response.data);
+      console.error("Response Error Status:", error.response.status);
+      console.error("Response Error Headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("Request Error:", error.request);
+    } else {
+      console.error("Setup Error:", error.message);
+    }
+  }
+}
+
+fetchDataAndSave();
